82975f37d2b74209b8a4ac7e62acc657049cc685
Jorge Ventura <jv120257@gmail.com>
2023-02-18 15:38:34 -0600

    New version

--- gentoo/rem-repo.sh
+++ gentoo/rem-repo.sh
@@ -5,11 +5,7 @@
 CMD=$1
 PARAM=$2
 
-if [ $CMD == "manifest" ]; then
+if [ $CMD == "ebuild" ]; then
    cp ./gentoo/hello.ebuild.in rem-repo/app-rem/hello/${PARAM}
-   ebuild ./rem-repo/app-rem/hello/${PARAM} manifest
-   cd ./rem-repo/
-   git add app-rem/hello/${PARAM}
-   git commit -am"New ebuild ${PARAM}"
 fi
 
