a0f25c018c39d53369441083d1fe3326a20b957c
Jorge Ventura <jv120257@gmail.com>
2023-02-18 19:53:38 -0600

    version 0.1.5

--- hello.cc
+++ hello.cc
@@ -6,6 +6,6 @@ hello world program
 #include "config.h"
 
 int main() {
-    std::cout << ">>> Hello World, " << "Version: " << VERSION << std::endl;
+    std::cout << ">>> Hello World <<<, " << "Version: " << VERSION << std::endl;
 }
 
